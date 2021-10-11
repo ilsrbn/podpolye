2c057c51e5afed25a6a1c0fe82f09bc8409d1970
ilsrbn <serbini271@gmail.com>
2021-10-11 11:52:11 +0300

    main page finis

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,66 @@
 <template>
-  <div>
+  <div class="boeng">
     <Hero />
     <LazyAbout />
     <LazyGallery />
     <BottomSplash />
+    <nuxt-link to="/events" class="sidelink" :class="isSide ? 'show': ''">
+      <img :src="require('~/assets/images/ANNOUNCE.png')" alt="" />
+    </nuxt-link>
   </div>
 </template>
 
-
-<style>
-body {
-  font-family: "Amatic SC", cursive;
-  background-color: #0d0d0d;
+<script>
+export default {
+  data:() => ({
+    isSide: false,
+  }),
+  methods: {
+    show() {
+      this.isSide = !this.isSide;
+    },
+    handleScroll() {
+        if (window.scrollY >= window.innerHeight * 0.4 && window.scrollY <= 1711) {
+          this.isSide = true;
+        } else {
+          this.isSide = false;
+        }
+    },
+  },
+  beforeMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
 }
+</script>
 
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
+<style lang="scss" scoped>
+.boeng {
+  position: relative;
+  .sidelink {
+    position: fixed;
+    left: 0;
+    top: 50%;
+    width: 11vw;
+    opacity: 0;
+    z-index: 1;
+    transition-property: opacity transform;
+    transition-duration: 100ms;
+    transition-timing-function: ease-in;
+    transform: translateX(-11vw);
+    img {
+      max-width: 100%;
+    }
+    &.show {
+      opacity: 1;
+      transform: translateX(0);
+      cursor: pointer;
+      &:hover {
+        opacity: 0.6;
+      }
+    }
+  }
 }
 </style>
