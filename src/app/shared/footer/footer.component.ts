f4b1ad92784592cc040dc8ff8ddaeaef14ca083e
ilsrbn <serbini271@gmail.com>
2026-04-27 09:56:01 +0300

    feat: FooterComponent with currentYear signal

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
+})
+export class FooterComponent {
+  readonly currentYear = signal(new Date().getFullYear());
+}
